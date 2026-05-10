6644565bfbf86dbe91759e839d069824e984299c
egnuyuki <n48691412921@gmail.com>
2026-05-10 20:38:29 +0900

    グラフ修正

--- front/src/components/AmbientGraph.jsx
+++ front/src/components/AmbientGraph.jsx
@@ -5,8 +5,6 @@ import {
   YAxis,
   Tooltip,
   ResponsiveContainer,
-  CartesianGrid,
-  LabelList,
 } from 'recharts'
 
 const AmbientGraph = ({ graphData, textColor }) => {
@@ -26,21 +24,21 @@ const AmbientGraph = ({ graphData, textColor }) => {
     };
 
   return (
-    <div className="fixed inset-0 z-10 pointer-events-none">
+    <div className="fixed inset-0 z-10" style={{ outline: 'none' }}>
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart
           data={graphData}
           margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
           onContextMenu={(_, e) => e.preventDefault()}
         >
-        <CartesianGrid strokeDasharray="4 4" stroke="#e5e7eb" />
+        {/* <CartesianGrid strokeDasharray="4 4" stroke="#e5e7eb" /> */}
           {/* 軸は非表示（アンビエントデザイン） */}
-          <XAxis dataKey="time" ticks={timeLabel} niceTicks="snap125" />
+          <XAxis dataKey="time" ticks={timeLabel} hide />
           {/* <YAxis domain={[0, 'auto']} hide /> */}
-          <YAxis domain={[dataMin => Math.floor(dataMin * 0.9), dataMax => Math.floor(dataMax * 1.1)]} niceTicks="snap125" hide/> // 最小値の10%下を基点
+          <YAxis domain={[dataMin => Math.floor(dataMin * 0.9), dataMax => Math.floor(dataMax * 1.1)]} hide/> // 最小値の10%下を基点
 
           {/* ツールチップも非表示 */}
-          <Tooltip active={true} />
+          <Tooltip active={true} contentStyle={{ backgroundColor: '#f5f5f5', borderColor: '#ccc' ,color: '#333'}} itemStyle={{ color: '#333' }} formatter={(value) => [`${value}°C`, null]} />
 
           <defs>
             <linearGradient id="tempGradient" x1="0" y1="0" x2="0" y2="1">
@@ -56,27 +54,15 @@ const AmbientGraph = ({ graphData, textColor }) => {
             strokeWidth={1.5}
             strokeOpacity={0.5}
             fill="url(#tempGradient)"
-            isAnimationActive={false}   // 再レンダリング時のアニメーションをOFF
+            isAnimationActive={true}   // 再レンダリング時のアニメーションをOFF
             dot={(props) => <CustomDot {...props} />}  // カスタムドットを使用
           >
-            <LabelList 
+            {/* <LabelList 
                 dataKey="temp" 
                 position="top" 
                 offset={10}
-                content={(props) => {
-                    const { x, y, value, index } = props;
-                    
-                    // 4時間ごとにラベルを表示（データは30分ごとなので、8時間ごとに表示する場合は index % 16 === 0）
-                    if (index % 4 === 0 && index !== 0) {
-                        return (
-                            <text x={x} y={y} dy={-20} fill="#666" fontSize={12} textAnchor="middle">
-                                {value}
-                            </text>
-                        );
-                    }
-                    return null;
-                }}
-            />
+                content={(props) => <RenderCustomizedLabel {...props} />}
+            /> */}
           </Area>
         </AreaChart>
       </ResponsiveContainer>
